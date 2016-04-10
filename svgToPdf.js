a0f43167979842a2dc58806ac7f81d2add598ee6
prasad4fun <hariprasad0994@gmail.com>
2016-04-10 23:29:34 +0530

    svgToPdfConverter

---
+++ svgToPdf.js
@@ -0,0 +1,43 @@
+function triggerSvgToPdf() {
+    svgToPdf(document.querySelector("div#graph svg"), function(pdf) {
+        download_pdf('SVG.pdf', pdf.output('dataurlstring'));
+    });
+};
+
+function svgToPdf(svg, callback) {
+
+    //If you just want a dataURI for an SVG,
+    //you can call svgAsDataUri with an SVG node, options,
+    //and a callback in which dataURI will be returned
+
+    svgAsDataUri(svg, {}, function(svg_uri) {
+        var image = document.createElement('img');
+
+        image.src = svg_uri;
+        image.onload = function() {
+            var canvas = document.createElement('canvas');
+            var context = canvas.getContext('2d');
+            var pdfDoc = new jsPDF('portrait', 'pt');
+            var dataUrl;
+
+            canvas.width = image.width;
+            canvas.height = image.height;
+            context.drawImage(image, 0, 0, image.width, image.height);
+            dataUrl = canvas.toDataURL('image/jpeg', 1.0); //fullQuality image
+            pdfDoc.addImage(dataUrl, 'JPEG', 0, 0, 600, 400);
+
+            callback(pdfDoc);
+        }
+    });
+}
+
+function download_pdf(name, dataUriString) {
+    var pdfDocLink = document.createElement('a');
+    pdfDocLink.addEventListener('click', function(ev) {
+        pdfDocLink.href = dataUriString;
+        pdfDocLink.download = name;
+        document.body.removeChild(link);
+    }, false);
+    document.body.appendChild(link);
+    pdfDocLink.click();
+}
